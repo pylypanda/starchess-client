7668c896fc7087fffc319ddc174efcd1bb7ca573
Andriy Pylypets <a.pylypets95@gmail.com>
2019-11-15 13:47:41 +0100

    Added l/t margin

--- src/pages/NoMatch/NotFound.js
+++ src/pages/NoMatch/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NoMatch = () => {
     return (
-        <h1>404 - Page Not Found</h1>
+        <h1 className='pl-5 pt-5 ml-5 mt-5'>404 - Page Not Found</h1>
     );
 };
 export default NoMatch;
