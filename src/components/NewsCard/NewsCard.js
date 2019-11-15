61638f871bbc520572dcb9e18eac9904d4169dec
Andriy Pylypets <a.pylypets95@gmail.com>
2019-11-15 13:53:18 +0100

    Added news text splitting into paragraphs

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -2,16 +2,23 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 
 const NewsCard = news => {
-    const { img, title, author, article, date } = news;
+    const { img, title, author, link, article, date } = news;
+    let articleChecked = [];
+    if(article)
+        articleChecked = article.split('||');
     return(
         <Card style={{backgroundColor: 'rgba(200, 200, 200, 0.9)'}}>
-            <Card.Img variant='top' src={img} fluid />
+            <Card.Img variant='top' src={img} />
             <Card.Body>
                 <Card.Title style={{color: 'black'}}>{title}</Card.Title>
                 <small className="text-muted mr-2">{date}</small>
-                <small className="text-muted">Author:&nbsp;<cite>{author}</cite></small>
+                <small className="text-muted">Author:&nbsp;<a href={link} className='news-author' target='_blank'><cite>{author}</cite></a></small>
                 <Card.Text style={{color: 'black'}}>
-                    {article}
+                    {articleChecked.map((par) => {
+                        return(
+                            <p>{par}</p>
+                        )
+                    })}
                 </Card.Text>
             </Card.Body>
         </Card>
