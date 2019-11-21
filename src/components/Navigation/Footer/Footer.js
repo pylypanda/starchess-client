00fc1a29bf55f7e7ce1d64d5b8eb0b308e4badf8
Andriy Pylypets <a.pylypets95@gmail.com>
2019-11-21 15:53:09 +0100

    Changed class components to functional

--- src/components/Navigation/Footer/Footer.js
+++ src/components/Navigation/Footer/Footer.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-class Footer extends Component {
-    render () {
-        return(
-            <div className='footer' style={{textAlign: 'center', fontSize: '14px'}}>
-                <NavLink to='/' className='px-2'>Home</NavLink>
-                <NavLink to='/' className='px-2'>About</NavLink>
-                <NavLink to='/contacts' className='px-2'>Contacts</NavLink>
-                <NavLink to='/' className='px-2'>Star Chess &copy; 2019</NavLink>
-            </div>
-        );
-    }
+const Footer = () => {
+    return(
+        <div className='footer' style={{textAlign: 'center', fontSize: '14px'}}>
+            <NavLink to='/' className='px-2'>Home</NavLink>
+            <NavLink to='/' className='px-2'>About</NavLink>
+            <NavLink to='/contacts' className='px-2'>Contacts</NavLink>
+            <NavLink to='/' className='px-2'>Star Chess &copy; 2019</NavLink>
+        </div>
+    );
 }
 
 export default Footer;
--- src/components/TopPlayersTable/TopPlayersTable.js
+++ src/components/TopPlayersTable/TopPlayersTable.js
@@ -1,14 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { Table } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSortDown } from '@fortawesome/free-solid-svg-icons';
 
 import './TopPlayersTable.css';
 
-class TopPlayersTable extends Component {
-    sortByName = (e) => {
+const TopPlayersTable = (props) => {
+    const sortByName = (e) => {
         e.preventDefault();
-        let top = this.props.top;
+        let top = props.top;
         top = top
             .slice(0)
             .sort((a, b) => {
@@ -16,23 +16,23 @@ class TopPlayersTable extends Component {
                 let y = b.name.toLowerCase();
                 return x < y ? -1 : x > y ? 1 : 0;
             });
-        this.props.sortTop([...top]);
+        props.sortTop([...top]);
     }
 
-    sortByRank = (e) => {
+    const sortByRank = (e) => {
         e.preventDefault();
-        let top = this.props.top;
+        let top = props.top;
         top = top
             .slice(0)
             .sort((a, b) => {
                 return a.rank - b.rank;
             });
-        this.props.sortTop([...top]);
+        props.sortTop([...top]);
     }
 
-    sortByCountry = (e) => {
+    const sortByCountry = (e) => {
         e.preventDefault();
-        let top = this.props.top;
+        let top = props.top;
         top = top
             .slice(0)
             .sort((a, b) => {
@@ -40,49 +40,45 @@ class TopPlayersTable extends Component {
                 let y = b.country.toLowerCase();
                 return x < y ? -1 : x > y ? 1 : 0;
             });
-            this.props.sortTop([...top]);
+            props.sortTop([...top]);
     }
 
-    sortByYear = (e) => {
+    const sortByYear = (e) => {
         e.preventDefault();
-        let top = this.props.top;
+        let top = props.top;
         top = top
             .slice(0)
             .sort((a, b) => {
                 return a.byear - b.byear;
             });
-        this.props.sortTop([...top]);
+        props.sortTop([...top]);
     }    
 
-    render() {
-        let top = this.props.top;
-        return(
-            <Table className='top-players-table' striped bordered size='sm' variant='dark'>
-                <thead>
-                    <tr style={{fontWeight: 'bold'}}>
-                        <td className='rank' onClick={(e) => this.sortByRank(e)} title='Sort by rank'>Rank <span className='d-none d-xl-inline'><FontAwesomeIcon icon={faSortDown} /></span></td>
-                        <td className='name' onClick={(e) => this.sortByName(e)} title='Sort by name'>Name <span className='d-none d-xl-inline'><FontAwesomeIcon icon={faSortDown} /></span></td>
-                        <td className='country' onClick={(e) => this.sortByCountry(e)} title='Sort by country'>Country <span className='d-none d-xl-inline'><FontAwesomeIcon icon={faSortDown} /></span></td>
-                        <td className='rating' onClick={(e) => this.sortByRank(e)} title='Sort by rating'>Rating <span className='d-none d-xl-inline'><FontAwesomeIcon icon={faSortDown} /></span></td>
-                        <td className='byear' onClick={(e) => this.sortByYear(e)} title='Sort by birthyear'>B&#8209;Year <span className='d-none d-xl-inline'><FontAwesomeIcon icon={faSortDown} /></span></td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {top.map(n => {
-                        return(
-                        <tr>
-                            <td className='rank'>{n.rank}</td>
-                            <td className='name'>{n.name}</td>
-                            <td className='country'>{n.country}</td>
-                            <td className='rating'>{n.rating}</td>
-                            <td className='byear'>{n.byear}</td>
-                        </tr>)
-                    })}
-                </tbody>
-            </Table>
-        )        
-    }
-
+    return(
+        <Table className='top-players-table' striped bordered size='sm' variant='dark'>
+            <thead>
+                <tr style={{fontWeight: 'bold'}}>
+                    <td className='rank' onClick={(e) => sortByRank(e)} title='Sort by rank'>Rank <span className='d-none d-xl-inline'><FontAwesomeIcon icon={faSortDown} /></span></td>
+                    <td className='name' onClick={(e) => sortByName(e)} title='Sort by name'>Name <span className='d-none d-xl-inline'><FontAwesomeIcon icon={faSortDown} /></span></td>
+                    <td className='country' onClick={(e) => sortByCountry(e)} title='Sort by country'>Country <span className='d-none d-xl-inline'><FontAwesomeIcon icon={faSortDown} /></span></td>
+                    <td className='rating' onClick={(e) => sortByRank(e)} title='Sort by rating'>Rating <span className='d-none d-xl-inline'><FontAwesomeIcon icon={faSortDown} /></span></td>
+                    <td className='byear' onClick={(e) => sortByYear(e)} title='Sort by birthyear'>B&#8209;Year <span className='d-none d-xl-inline'><FontAwesomeIcon icon={faSortDown} /></span></td>
+                </tr>
+            </thead>
+            <tbody>
+                {props.top.map(n => {
+                    return(
+                    <tr>
+                        <td className='rank'>{n.rank}</td>
+                        <td className='name'>{n.name}</td>
+                        <td className='country'>{n.country}</td>
+                        <td className='rating'>{n.rating}</td>
+                        <td className='byear'>{n.byear}</td>
+                    </tr>)
+                })}
+            </tbody>
+        </Table>
+    )
 }
 
 export default TopPlayersTable;
